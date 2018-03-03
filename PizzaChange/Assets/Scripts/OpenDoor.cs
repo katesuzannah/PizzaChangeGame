71e27c49c921bc62aee3c36b2f8e94c1a2af04e1
Unknown <katesuzannah@gmail.com>
2018-03-03 14:11:39 -0500

    Greybox blockout and initial scripts

---
+++ PizzaChange/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour {
+
+	public bool open;
+	bool moving;
+	float distCovered;
+	float fracJourney;
+	public float rotSpeed;
+	float startTime;
+	float journeyLength;
+	public Vector3 openRotation;
+	Vector3 closedRotation;
+	Vector3 targetRotation;
+
+	void Start () {
+		closedRotation = transform.localEulerAngles;
+		moving = false;
+	}
+
+	void Update () {
+		if (moving) {
+			distCovered = (Time.time - startTime) * rotSpeed;
+			fracJourney = distCovered / journeyLength;
+			transform.localRotation = Quaternion.Slerp (transform.localRotation, Quaternion.Euler (targetRotation), fracJourney);
+			//transform.localEulerAngles = Vector3.Lerp (transform.localEulerAngles, targetRotation, fracJourney);
+			if (Quaternion.Angle(transform.localRotation, Quaternion.Euler(targetRotation)) < .001f) {
+				moving = false;
+			}
+//			if (Vector3.Distance (transform.localEulerAngles, targetRotation) < .001f) {
+//				moving = false;
+//			}
+		}
+	}
+
+	public void OnClick () {
+		startTime = Time.time;
+		if (open) {
+			//Close
+			targetRotation = closedRotation;
+			open = false;
+		}
+		else {
+			//Open
+			targetRotation = openRotation;
+			open = true;
+		}
+		journeyLength = Vector3.Distance (transform.localEulerAngles, targetRotation);
+		moving = true;
+	}
+}
---
+++ PizzaChange/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+	CharacterController playerCharCon;
+
+	Vector3 moveVector;
+	public float moveSpeed = 5f;
+	public float crouchY;
+	float standY;
+	float distCovered;
+	float fracJourney;
+	float journeyLength;
+	float startTime;
+	public float crouchLerpSpeed;
+
+	void Start () {
+		playerCharCon = GetComponent<CharacterController>();
+		standY = Camera.main.transform.localPosition.y;
+	}
+	
+
+	void Update () {
+		float frontBack = Input.GetAxis("Vertical") * moveSpeed;
+		float leftRight = Input.GetAxis("Horizontal") * moveSpeed;
+		playerCharCon.Move(transform.forward * frontBack * Time.deltaTime);
+		playerCharCon.Move(transform.right * leftRight * Time.deltaTime);
+		// Add gravity
+		playerCharCon.Move(Physics.gravity*.008f); //move the controller down
+
+		//Crouch
+		if (Input.GetKeyDown(KeyCode.C)) {
+			startTime = Time.time;
+			journeyLength = Vector3.Distance (Camera.main.transform.localPosition, new Vector3 (Camera.main.transform.localPosition.x, crouchY, Camera.main.transform.localPosition.z));
+		}
+		if (Input.GetKey(KeyCode.C)) {
+			//Camera.main.transform.localPosition = new Vector3 (Camera.main.transform.localPosition.x, crouchY, Camera.main.transform.localPosition.z);
+			distCovered = (Time.time - startTime) * crouchLerpSpeed;
+			fracJourney = distCovered / journeyLength;
+			Camera.main.transform.localPosition = Vector3.Lerp (Camera.main.transform.localPosition, new Vector3 (Camera.main.transform.localPosition.x, crouchY, Camera.main.transform.localPosition.z), fracJourney);
+		}
+		else {
+			Camera.main.transform.localPosition = new Vector3 (Camera.main.transform.localPosition.x, standY, Camera.main.transform.localPosition.z);
+		}
+	}
+}
