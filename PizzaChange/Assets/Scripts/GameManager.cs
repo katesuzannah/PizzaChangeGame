2c4337ac1d5cc4caa7f265c996afbd0940ef46b0
Unknown <katesuzannah@gmail.com>
2018-03-03 18:09:18 -0500

    More stuffs idk what I did

--- PizzaChange/Assets/Scripts/GameManager.cs
+++ PizzaChange/Assets/Scripts/GameManager.cs
@@ -9,15 +9,27 @@ public class GameManager : MonoBehaviour {
 	public Text scoreDisplay;
 	float timer;
 	public Text timerDisplay;
+	public AudioSource playerAudio;
+	public AudioSource doorAudio;
+	public AudioClip moneyAudio;
+	bool doorbellRang;
 
 	void Start () {
 		score = 0f;
 		timer = 60f;
+		doorbellRang = false;
 	}
 
 	void Update () {
 		scoreDisplay.text = "$" + score;
-		timer -= Time.deltaTime;
 		timerDisplay.text = "" + timer;
+		if (timer > 0f) {
+			timer -= Time.deltaTime;
+		}
+		else {
+			if (!doorbellRang) {
+				doorAudio.Play ();
+			}
+		}
 	}
 }
